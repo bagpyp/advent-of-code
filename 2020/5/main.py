c5e0846332cfff6db0da6efbfe8805b29826c0eb
Robert Cunningham <rcunni64@ford.com>
2021-11-14 13:16:02 -0800

    comment code in 5

--- 2020/5/main.py
+++ 2020/5/main.py
@@ -1,6 +1,4 @@
 #%%
-import re
-
 with open('input') as f:
     seats = [
         (
@@ -8,12 +6,18 @@ with open('input') as f:
             s[7:].replace('L','0').replace('R','1')
         ) for s in f.read().split('\n')
     ]
-    # ..., ('FBBFFBF', 'LLR')]
-    
+    # ..., ('FBBFFBF', 'LLR')] ->
+    # ..., ('0110010', '001')]
+
+# it's binary!
 ids = [8*int(s[0],2) + int(s[1],2) for s in seats]
+# >>> int('111', 2)
+# 7    
 
-i = max(ids)
+# part 1
+print(max(ids))
 
+# part 2
 for n in range(min(ids),max(ids)+1):
     if n not in ids:
         print(n)

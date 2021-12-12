d2b09c0f9e058c8d6943b294470d616151f29ba7
Robert Cunningham <rcunni64@ford.com>
2021-12-11 17:00:09 -0800

    finish 6.1

---
+++ 2021/6 lanternfish/1.py
@@ -0,0 +1,36 @@
+#%%
+with open ('input') as f:
+    input = [int(l) for l in f.read().split(',')]
+
+class Fish:
+    def __init__(self, timer: int = 8) -> None:
+        self.timer = timer
+    def __repr__(self) -> str:
+        return str(self.timer)
+    def wake_up(self):
+        if self.timer < 7:
+            self.timer = (self.timer - 1) % 7
+        else:
+            self.timer -= 1
+
+class School:
+    def __init__(self, fish: list[Fish]) -> None:
+        self.fish = fish
+    def __repr__(self) -> str:
+        return str(self.fish)
+    def cycle(self):
+        bursters = [f.timer for f in self.fish].count(0)
+        for f in self.fish:
+            f.wake_up()
+        self.fish.extend([Fish() for _ in range(bursters)])
+    def count_fish(self):
+        return len(self.fish)
+
+fish = [Fish(i) for i in input]
+school = School(fish)
+
+for _ in range(80):
+    school.cycle()
+
+print(school.count_fish())
+# %%
